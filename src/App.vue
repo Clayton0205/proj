1b2b127ac9718674b364243df25a577dfbd6d690
Clayton0205 <claytonadonis5@gmail.com>
2022-08-02 13:53:59 +0200

    HomeB

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,51 @@
 <template>
-<nav id="navbar">
-  <router-link to="/" class="link">Home</router-link>
-  <p class="link"> | </p>
-  <router-link to="/about" class="link">About</router-link>
-</nav>
-  <router-view/>
+  <nav id="navbar">
+    <router-link to="/" class="link" id="home">Home</router-link>
+    <p id="line"> | </p>
+    <router-link to="/about" class="link" id="about">About</router-link>
+  </nav>
+
+  <router-view />
 </template>
 
 <style scoped>
-*{
+* {
   margin: 0;
   padding: 0;
 }
 
-#navbar{
-  width: 220px;
+#navbar {
+  width: 100%;
   height: 40px;
+  margin: auto;
+  padding-top: 2px;
+  display: flex;
+  justify-content: center;
+  background-image: linear-gradient(to right,  rgb(5, 52, 93), rgb(182, 13, 74));
+}
+
+.link {
+  margin: 0px 40px 0px 0px;
   margin: auto;
   display: flex;
   justify-content: center;
-  border: 1px solid rgb(5, 52, 93);
-  border-radius: 3px;
+  text-decoration: none;
+  color: white;
 }
 
-.link{
-  margin: 0px 40px 0px 0px;
-    margin: auto;
-  display: flex;
-  justify-content: center;
-  text-decoration: none;
+.link:hover {
+  color: rgb(5, 52, 93);
+}
+
+#home:hover{
 color: rgb(182, 13, 74);
 }
 
-.link:hover{
-color: rgb(5, 52, 93);
-font-size: 25px;
+#line{
+  color: white;
+}
+
+#about:hover{
+  color: rgb(5, 52, 93);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <template>
     <div id="page">
-       <img src="../assets/PNP.jpg" id="logo">
+        <div class="container">
+            <div class="row">
+                <img src="../assets/PNP.jpg" id="logo">
+            </div>
+            <div class="row">
+                <h1>“Inspired by you”</h1>
+            </div>
+        </div>
     </div>
+            <footer id="footer">
+            <p>color: rgb(182, 13, 74);</p>
+        </footer>
 </template>
 
 <style scoped>
@@ -10,15 +20,28 @@
     padding: 0;
 }
 
-#page{
+#page {
+    width: 100%;
+    height: 728px;
     display: flex;
     justify-content: center;
-
+   background-image: linear-gradient(to right,  rgb(5, 52, 93), rgb(182, 13, 74));
 }
 
-#logo{
+#logo {
     margin: auto;
     width: 800px;
     height: 500px;
+}
+
+.row {
+    text-align: center;
+    color: white;
+}
+
+#footer {
+background-image: linear-gradient(to right,  rgb(5, 52, 93), rgb(182, 13, 74));
+   color: white;
+   text-align: center;
 }
 </style>
